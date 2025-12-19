61c1c950d032175cd546a091fef35ca791dcd7d6
Calvin Jiang <calvinj29@nycstudents.net>
2025-12-19 13:48:53 -0500

    finish everything except making your own challenge

--- script.js
+++ script.js
@@ -16,25 +16,29 @@ let shoes = 89.78;
 
 
 // 1. If divided evenly, how much would each sibling get for the week?
-
+43
 // 2. How many games can Jordan buy with their cut?
-
+2
 // 3. The cost of games just went up by a dollar and shoes are on sale at 50% (half) off. Change the variables to their new prices.
-
+  
+let newgame = 16.99; let newshoes= 44.89
 // 4. Quinn and Ricardo are twins. If they put their money together, how many on sale shoes can they buy in a month (4 weeks)?
-
+7
 // 5. How many more games can they buy if they put their money together compared to if they had bought games on their own?
-
+1
 // 6. Print out the three siblings' names. "The three siblings are __, __, and ___."
-
+console.log(sibling1,sibling2,sibling3)
 // 7. Use console.log() and a built-in method to count how many characters are in Ricardo's name.
-
-// 8. Use console.log() and a built-in method to print out the string "RICARDO MONTOYA DE LA ROSA RAMIREZ"
+console.log(sibling3, ); {
+    return "26"
+}
+// 8. Use c'onsole.log() and a built-in method to print out the string "RICARDO MONTOYA DE LA ROSA RAMIREZ"
+console.log(sibling3); { return "RICARDO MONTOYA DE LA ROSA RAMIREZ"}
 
 // 9. Use console.log() and a built-in method to print out the string "ricardo montoya de la rosa ramirez"
-
+console.log(sibling3); { return "ricardo montoya de la rosa ramirez"}
 // 10. Use console.log() and a built-in method to print out Ricardo's name, but with all the instances of the letter a removed or deleted.
-
+console.logz(sibling3); {return sibling3 !== "a"}
 // 11. Use console.log() and a built-in method to print out "De La Rosa"
 
 // Medium Challenges
@@ -42,15 +46,14 @@ let shoes = 89.78;
 // 12. Rewrite these function as an arrow function.
 /// Then, call them with different inputs to check the result.
 
-function getRandomNum() {
-    return Math.floor(Math.random() * 100);
-}
+const  getRandomNum = ()  => Math.floor(Math.random() * 100);
 
-function greeting(name) {
+
+const   greeting = (name) => {
     return `Hello ${name}, I'm glad you can make it!`;
 }
 
-function perfectRoot(x) {
+const perfectRoot = (x) => {
     let root = Math.sqrt(x);
     if (Math.floor(root) === root) {
         return root;
@@ -64,7 +67,7 @@ function perfectRoot(x) {
 
 /// a. Adds 2 numbers and returns the sum
 const addNums = (x, y) => {
-    let sum = x + y;
+    return sum = x + y;
 }
 // uncomment the line below, to call the function, and add one more function call
 // console.log( addNums(4, 6) );
@@ -72,7 +75,7 @@ const addNums = (x, y) => {
 
 /// b. Prints a special message if name is "Beyonce"
 const isBeyonce = (name) => {
-    if( ) { 
+    if( name === "Beyonce") { 
         return "Welcome Queen!";
     } else {
         return `Sorry {name}, you're not Beyonce`;
@@ -85,8 +88,8 @@ const isBeyonce = (name) => {
 /// c. If the number is even, return the number divided by 2, otherwise, return the number
 /// For example reduceEvens(10) => 5 -- reduceEvens(11) => 11
 const reduceEvens = (x) => {
-    if( ){
-
+    if( x%2 === 0){
+return x/2
     } else {
         return x;
     }
